8a3b50f240699ed6093eb3d7d01b1a26a450760a
thiago_osim <thiago_osim@hotmail.com>
2024-08-14 21:25:31 -0300

    Criado a implementação do método getWeatherDatas da classe weatherService

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = 'São Paulo';
 
+  constructor(private weatherService: WeatherService) {}
+
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName)
+    .subscribe({
+      next: (response) => {
+        console.log(response)
+      },
+      error: (error) => console.log(error),
+    });
+  }
 }
