490004f56fb93dfd7c01eb1b8af44b14026abb73
thiago_osim <thiago_osim@hotmail.com>
2024-08-14 21:09:43 -0300

    Criado chamada da API open weather

--- src/app/modules/weather/services/weather.service.ts
+++ src/app/modules/weather/services/weather.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  private apiKey = 'b5d6cb109c4bd6057ef399d787db4ed2';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getWeatherDatas(cityName: string): Observable<any> {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&mode=json&APPID=${this.apiKey}`, {});
+  }
 }
