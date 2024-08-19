47039512692422e6b3104e1c606948576bac9226
thiago_osim <thiago_osim@hotmail.com>
2024-08-19 08:42:11 -0300

    Adicionado lógica de passagem de dados de componente pai para filho

--- src/app/modules/weather/components/weather-card/weather-card.component.ts
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { weatherDatas } from 'src/app/models/interfaces/weatherDatas';
 
 @Component({
   selector: 'app-weather-card',
   templateUrl: './weather-card.component.html',
   styleUrls: []
 })
-export class WeatherCardComponent {
+export class WeatherCardComponent implements OnInit {
+  @Input() weatherDatasInput!: weatherDatas;
 
+  ngOnInit(): void {
+    console.log('DADOS RECEBIDOS DO COMPONENTE PAI - WEATHER-CARD: ', this.weatherDatasInput);
+  }
 }
